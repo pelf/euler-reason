ef60a8c1db0dfdf01dd25d608efe88c55d6a2029
Pedro Freitas <dryginlogic@gmail.com>
2016-11-02 14:11:34 +0000

    Use ocamlfind to load modules correctly

--- exec.sh
+++ exec.sh
@@ -5,7 +5,7 @@ eval $(./node_modules/.bin/dependencyEnv)
 nopam
 
 echo "Building..."
-rebuild -I src ./src/$1.native 2>&1
+rebuild -use-ocamlfind -I src ./src/$1.native 2>&1
 
 echo "Running"...
 ./_build/src/$1.native 2>&1
---
+++ src/TestCore.re
@@ -0,0 +1,4 @@
+open Core.Std;
+open Big_int;
+
+print_endline (string_of_big_int (big_int_of_int 1));
