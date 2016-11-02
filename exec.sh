228bf374ae3d9b1b7936b27deacb089a29b28881
Pedro Freitas <dryginlogic@gmail.com>
2016-11-02 15:51:27 +0000

    `refmterr` processes stderr to print type errors more beautifully

--- exec.sh
+++ exec.sh
@@ -5,7 +5,7 @@ eval $(./node_modules/.bin/dependencyEnv)
 nopam
 
 echo "Building..."
-rebuild -use-ocamlfind -I src ./src/$1.native 2>&1
+rebuild -use-ocamlfind -I src ./src/$1.native 2>&1 | refmterr
 
 echo "Running"...
 ./_build/src/$1.native 2>&1
